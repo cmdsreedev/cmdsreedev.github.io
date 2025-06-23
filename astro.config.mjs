885db0cfedcea7988006be3e0f8ef71071d23256
cmdsree <asreevisakh@gmail.com>
2025-06-22 19:21:17 -0700

    fix: fix site config

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
-
-export default defineConfig({
-  site: "https://cmdsreedev.github.io",
-  base: "/",
-  integrations: [github()],
-});
